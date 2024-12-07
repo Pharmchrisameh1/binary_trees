3ee9a51e105dab5b1876368864cb110cf958ad3d
Rx Chris Ameh <chris1jnr@gmail.com>
2024-12-07 01:26:04 -0800

    132-array_to_heap.c

--- binary_trees.h
+++ binary_trees.h
@@ -68,7 +68,8 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
-binary_tree_t *heap_insert(binary_tree_t **root, int value); /* Added function */
+binary_tree_t *heap_insert(binary_tree_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size); /* Newly added function */
 
 #endif /* BINARY_TREES_H */
 
